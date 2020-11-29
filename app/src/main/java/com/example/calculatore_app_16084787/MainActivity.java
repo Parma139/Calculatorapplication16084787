1e0e707c6f7c2b348d636646f2a1afbffa24035c
Parma139 <paramraj139@gmail.com>
2020-11-29 19:00:39 +0000

    Uploading on git hub
    This version is mess but try to check file are added on git hub properly.
    
    (Code have object layout plate and apple)

---
+++ app/src/main/java/com/example/calculatore_app_16084787/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.calculatore_app_16084787;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
